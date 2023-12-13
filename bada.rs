fee95b90e5cfcdebcdeb2f899c6ea8f617fb3133
rexim <reximkut@gmail.com>
2023-12-13 22:45:44 +0700

    Implement traditional compiler modules
    - mod diag
    - mod lex
    - mod parser

--- bada.rs
+++ bada.rs
@@ -75,7 +75,7 @@ mod asm {
     //   Code:(ChunkSize-SubSize)/binary,  % all remaining data
     //   Padding4:0..3/unit:8
     // >>
-    pub fn encode_code_chunk<'a>(program: &HashMap<&'a str, usize>, atoms: &mut Atoms<'a>, labels: &mut HashMap<u32, u32>) -> Vec<u8> {
+    pub fn encode_code_chunk<'a>(program: &'a HashMap<String, usize>, atoms: &mut Atoms<'a>, labels: &mut HashMap<u32, u32>) -> Vec<u8> {
         let mut label_count: u32 = 0;
         let mut function_count: u32 = 0;
 
@@ -89,7 +89,7 @@ mod asm {
 
             code.push(OpCode::FuncInfo as u8);
             code.extend(encode_arg(Tag::A, atoms.get_id("bada") as i32));
-            let name_id = atoms.get_id(name);
+            let name_id = atoms.get_id(&name);
             code.extend(encode_arg(Tag::A, name_id as i32));
             code.extend(encode_arg(Tag::U, 0));
 
@@ -215,6 +215,222 @@ mod asm {
     }
 }
 
+#[macro_use]
+mod diag {
+    pub struct Loc {
+        pub file_path: String,
+        pub row: usize,
+        pub col: usize,
+    }
+
+    macro_rules! report {
+        ($loc:expr, $level:literal, $fmt:literal) => {
+            let diag::Loc{file_path, row, col} = $loc;
+            let level = $level;
+            eprint!("{file_path}:{row}:{col}: {level}: ");
+            eprintln!($fmt);
+        };
+        ($loc:expr, $level:literal, $fmt:literal, $($args:tt)*) => {
+            let Loc{file_path, row, col} = $loc;
+            let level = $level;
+            eprint!("{file_path}:{row}:{col}: {level}: ");
+            eprintln!($fmt, $($args)*);
+        };
+    }
+}
+
+mod lex {
+    use diag::Loc;
+
+    #[derive(PartialEq)]
+    pub enum TokenKind {
+        Ident,
+        Equals,
+        Number,
+        End,
+        Unknown
+    }
+
+    impl TokenKind {
+        fn human(&self) -> &str {
+            match self {
+                Self::Ident => "identifier",
+                Self::Equals => "equals sign",
+                Self::Number => "number",
+                Self::End => "end of input",
+                Self::Unknown => "unknown token",
+            }
+        }
+    }
+
+    pub struct Token {
+        pub kind: TokenKind,
+        pub text: String,
+        pub loc: Loc,
+    }
+
+    pub struct Lexer<'a> {
+        content: &'a [char],
+        file_path: String,
+        pos: usize,
+        bol: usize,
+        row: usize,
+    }
+
+    impl<'a> Lexer<'a> {
+        pub fn new(content: &'a [char], file_path: String) -> Self {
+            Self {content, file_path, pos: 0, bol: 0, row: 0}
+        }
+
+        pub fn expect_tokens(&mut self, expected_kinds: &[TokenKind]) -> Option<Token> {
+            let token = self.next_token();
+            for kind in expected_kinds {
+                if token.kind == *kind {
+                    return Some(token)
+                }
+            }
+
+            let mut expected_list = String::new();
+            for (i, kind) in expected_kinds.iter().enumerate() {
+                if i == 0 {
+                    expected_list.push_str(&format!("{}", kind.human()))
+                } else if i + 1 >= expected_kinds.len() {
+                    expected_list.push_str(&format!(", or {}", kind.human()))
+                } else {
+                    expected_list.push_str(&format!(", {}", kind.human()))
+                }
+            }
+
+            report!(token.loc, "ERROR", "Expected {expected_list}, but got {actual}",
+                    actual = token.kind.human());
+            None
+        }
+
+        pub fn next_token(&mut self) -> Token {
+            self.trim_whitespaces();
+
+            let loc = Loc {
+                file_path: self.file_path.clone(),
+                row: self.row + 1,
+                col: self.pos - self.bol + 1,
+            };
+
+            let x = if let Some(x) = self.current_char() {
+                x
+            } else {
+                return Token {
+                    text: "".to_string(),
+                    loc,
+                    kind: TokenKind::End,
+                }
+            };
+
+            if x.is_alphabetic() {
+                let mut text = String::new();
+                while let Some(x) = self.current_char() {
+                    if x.is_alphanumeric() {
+                        self.chop_char();
+                        text.push(x);
+                    } else {
+                        break;
+                    }
+                }
+                return Token {
+                    text,
+                    loc,
+                    kind: TokenKind::Ident,
+                }
+            }
+
+            if x.is_numeric() {
+                let mut text = String::new();
+                while let Some(x) = self.current_char() {
+                    if x.is_numeric() {
+                        self.chop_char();
+                        text.push(x);
+                    } else {
+                        break;
+                    }
+                }
+                return Token {
+                    text,
+                    loc,
+                    kind: TokenKind::Number,
+                }
+            }
+
+            match x {
+                '=' => {
+                    self.chop_char();
+                    return Token {
+                        text: x.to_string(),
+                        loc,
+                        kind: TokenKind::Equals,
+                    }
+                }
+                _ => {}
+            }
+
+            self.chop_char();
+            Token {
+                text: x.to_string(),
+                loc,
+                kind: TokenKind::Unknown,
+            }
+        }
+
+        fn trim_whitespaces(&mut self) {
+            while self.current_char().map(|x| x.is_whitespace()).unwrap_or(false) {
+                self.chop_char();
+            }
+        }
+
+        fn current_char(&self) -> Option<char> {
+            self.content.get(self.pos).cloned()
+        }
+
+        fn chop_char(&mut self) {
+            if let Some(x) = self.current_char() {
+                self.pos += 1;
+                if x == '\n' {
+                    self.row += 1;
+                    self.bol = self.pos;
+                }
+            }
+        }
+    }
+}
+
+mod parser {
+    use diag;
+    use lex;
+    use std::collections::HashMap;
+
+    pub fn parse_program(lexer: &mut lex::Lexer) -> Option<HashMap<String, usize>> {
+        let mut program: HashMap<String, usize> = HashMap::new();
+        loop {
+            let ident = lexer.expect_tokens(&[lex::TokenKind::Ident, lex::TokenKind::End])?;
+            match ident.kind {
+                lex::TokenKind::Ident => {
+                    let _ = lexer.expect_tokens(&[lex::TokenKind::Equals])?;
+                    let number = lexer.expect_tokens(&[lex::TokenKind::Number])?;
+                    match number.text.parse::<usize>() {
+                        Ok(x) => {
+                            program.insert(ident.text, x);
+                        }
+                        Err(err) => {
+                            report!(&number.loc, "ERROR", "Could not parse number: {err}");
+                            return None
+                        }
+                    }
+                }
+                lex::TokenKind::End => return Some(program),
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
 fn main() -> ExitCode {
     let mut args = env::args();
     let program = args.next().expect("program");
@@ -229,24 +445,19 @@ fn main() -> ExitCode {
 
     let output_path = Path::new(&input_path).with_extension("beam");
 
-    let content = match fs::read_to_string(&input_path) {
-        Ok(content) => content,
+    let content: Vec<_> = match fs::read_to_string(&input_path) {
+        Ok(content) => content.chars().collect(),
         Err(err) => {
             eprintln!("ERROR: could not load file {input_path}: {err}");
             return ExitCode::FAILURE;
         }
     };
-    let mut program: HashMap<&str, usize> = HashMap::new();
-    for line in content.lines() {
-        if line.len() > 0 {
-            let chunks: Vec<_> = line.split("=").collect();
-            if let &[key, value] = &chunks[..] {
-                let key = key.trim();
-                let value = value.trim().parse::<usize>().unwrap();
-                program.insert(key, value);
-            }
-        }
-    }
+    let mut lexer = lex::Lexer::new(&content, input_path.clone());
+    let program = if let Some(program) = parser::parse_program(&mut lexer) {
+        program
+    } else {
+        return ExitCode::FAILURE;
+    };
 
     let mut atoms = asm::Atoms::default();
     let mut labels: HashMap<u32, u32> = HashMap::new();
