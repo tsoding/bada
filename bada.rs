667684f08af39265c332507aa119f798084320b5
rexim <reximkut@gmail.com>
2023-12-13 18:52:23 +0700

    Factor out asm module

--- bada.rs
+++ bada.rs
@@ -4,209 +4,213 @@ use std::env;
 use std::path::Path;
 use std::process::ExitCode;
 
-#[repr(u8)]
-enum Tag {
-    U = 0,
-    I = 1,
-    A = 2,
-    X = 3,
-    // Y = 4,
-    // F = 5,
-    // H = 6,
-    // Z = 7,
-}
+mod asm {
+    use std::collections::HashMap;
 
-fn encode_arg(tag: Tag, n: i32) -> Vec<u8> {
-    if n < 0 {
-        todo!("negative");
-    } else if n < 16 {
-        // (N bsl 4) bor Tag;
-        let tag = tag as u8;
-        let n = n as u8;
-        vec![(n<<4)|tag]
-    } else if n < 0x800 {
-        // [((N bsr 3) band 2#11100000) bor Tag bor 2#00001000, N band 16#ff];
-        let tag = tag as u32;
-        let n = n as u32;
-        let a = (((n>>3)&0b11100000u32)|tag|0b00001000u32) as u8;
-        let b = (n&0xFF) as u8;
-        vec![a, b]
-    } else {
-        todo!("large numbers");
-    }
-}
-
-#[repr(u8)]
-enum OpCode {
-    Label = 1,
-    FuncInfo = 2,
-    IntCodeEnd = 3,
-    Return = 19,
-    Move = 64,
-}
-
-// aaaa|aaaa|a000|
-fn pad_chunk(word_size: usize, chunk: &mut Vec<u8>) {
-    let len = chunk.len();
-    let new_len = (len + word_size - 1)/word_size*word_size;
-    chunk.resize(new_len, 0)
-}
-
-fn encode_chunk(tag: [u8; 4], chunk: Vec<u8>) -> Vec<u8> {
-    let mut result = Vec::new();
-    result.extend(tag);
-    result.extend((chunk.len() as u32).to_be_bytes());
-    result.extend(chunk);
-    pad_chunk(4, &mut result);
-    result
-}
-
-// CodeChunk = <<
-//   ChunkName:4/unit:8 = "Code",
-//   ChunkSize:32/big,
-//   SubSize:32/big,
-//   InstructionSet:32/big,        % Must match code version in the emulator
-//   OpcodeMax:32/big,
-//   LabelCount:32/big,
-//   FunctionCount:32/big,
-//   Code:(ChunkSize-SubSize)/binary,  % all remaining data
-//   Padding4:0..3/unit:8
-// >>
-fn encode_code_chunk<'a>(program: &HashMap<&'a str, usize>, atoms: &mut Atoms<'a>, labels: &mut HashMap<u32, u32>) -> Vec<u8> {
-    let mut label_count: u32 = 0;
-    let mut function_count: u32 = 0;
-
-    let mut code = Vec::new();
-    for (name, result) in program.iter() {
-        function_count += 1;
-
-        label_count += 1;
-        code.push(OpCode::Label as u8);
-        code.extend(encode_arg(Tag::U, label_count as i32));
-
-        code.push(OpCode::FuncInfo as u8);
-        code.extend(encode_arg(Tag::A, atoms.get_id("bada") as i32));
-        let name_id = atoms.get_id(name);
-        code.extend(encode_arg(Tag::A, name_id as i32));
-        code.extend(encode_arg(Tag::U, 0));
-
-        label_count += 1;
-        code.push(OpCode::Label as u8);
-        code.extend(encode_arg(Tag::U, label_count as i32));
-        labels.insert(name_id as u32, label_count);
-
-        code.push(OpCode::Move as u8);
-        code.extend(encode_arg(Tag::I, (*result) as i32));
-        code.extend(encode_arg(Tag::X, 0));
-
-        code.push(OpCode::Return as u8);
-    }
-    code.push(OpCode::IntCodeEnd as u8);
-
-    label_count += 1;
-
-    let sub_size: u32 = 16;
-    let instruction_set: u32 = 0;
-    let opcode_max: u32 = 169;
-
-    let mut chunk = Vec::new();
-    chunk.extend(sub_size.to_be_bytes());
-    chunk.extend(instruction_set.to_be_bytes());
-    chunk.extend(opcode_max.to_be_bytes());
-    chunk.extend(label_count.to_be_bytes());
-    chunk.extend(function_count.to_be_bytes());
-    chunk.extend(code);
-
-    encode_chunk(*b"Code", chunk)
-}
-
-// AtomChunk = <<
-//   ChunkName:4/unit:8 = "Atom" | "AtU8",
-//   ChunkSize:32/big,
-//   NumberOfAtoms:32/big,
-//   [<<AtomLength:8, AtomName:AtomLength/unit:8>> || repeat NumberOfAtoms],
-//   Padding4:0..3/unit:8
-// >>
-fn encode_atom_chunk(atoms: &Atoms) -> Vec<u8> {
-    let mut chunk = Vec::new();
-    chunk.extend((atoms.names.len() as u32).to_be_bytes());
-    for atom in atoms.names.iter() {
-        chunk.extend((atom.len() as u8).to_be_bytes());
-        chunk.extend(atom.as_bytes());
+    #[repr(u8)]
+    enum Tag {
+        U = 0,
+        I = 1,
+        A = 2,
+        X = 3,
+        // Y = 4,
+        // F = 5,
+        // H = 6,
+        // Z = 7,
     }
 
-    encode_chunk(*b"AtU8", chunk)
-}
-
-// ImportChunk = <<
-//   ChunkName:4/unit:8 = "ImpT",
-//   ChunkSize:32/big,
-//   ImportCount:32/big,
-//   [ << ModuleName:32/big,
-//        FunctionName:32/big,
-//        Arity:32/big
-//     >> || repeat ImportCount ],
-//   Padding4:0..3/unit:8
-// >>
-fn encode_imports_chunk() -> Vec<u8> {
-    let mut chunk = Vec::new();
-    let import_count: u32 = 0;
-    chunk.extend(import_count.to_be_bytes());
-
-    encode_chunk(*b"ImpT", chunk)
-}
-
-// ExportChunk = <<
-//   ChunkName:4/unit:8 = "ExpT",
-//   ChunkSize:32/big,
-//   ExportCount:32/big,
-//   [ << FunctionName:32/big,
-//        Arity:32/big,
-//        Label:32/big
-//     >> || repeat ExportCount ],
-//   Padding4:0..3/unit:8
-// >>
-fn encode_exports_chunk(labels: &HashMap<u32, u32>) -> Vec<u8> {
-    let mut chunk = Vec::new();
-    let export_count: u32 = labels.len() as u32;
-    chunk.extend(export_count.to_be_bytes());
-
-    for (name_id, label) in labels.iter() {
-        chunk.extend(name_id.to_be_bytes());
-        chunk.extend(0u32.to_be_bytes());
-        chunk.extend(label.to_be_bytes());
-    }
-
-    encode_chunk(*b"ExpT", chunk)
-}
-
-// StringChunk = <<
-//   ChunkName:4/unit:8 = "StrT",
-//   ChunkSize:32/big,
-//   Data:ChunkSize/binary,
-//   Padding4:0..3/unit:8
-// >>
-fn encode_string_chunk() -> Vec<u8> {
-    encode_chunk(*b"StrT", vec![])
-}
-
-#[derive(Default)]
-struct Atoms<'a> {
-    names: Vec<&'a str>,
-}
-
-impl<'a> Atoms<'a> {
-    fn get_id(&mut self, needle: &'a str) -> usize {
-        let result = self.names
-            .iter()
-            .enumerate()
-            .find(|(_, &name)| name == needle)
-            .map(|(index, _)| index + 1);
-        if let Some(id) = result {
-            id
+    fn encode_arg(tag: Tag, n: i32) -> Vec<u8> {
+        if n < 0 {
+            todo!("negative");
+        } else if n < 16 {
+            // (N bsl 4) bor Tag;
+            let tag = tag as u8;
+            let n = n as u8;
+            vec![(n<<4)|tag]
+        } else if n < 0x800 {
+            // [((N bsr 3) band 2#11100000) bor Tag bor 2#00001000, N band 16#ff];
+            let tag = tag as u32;
+            let n = n as u32;
+            let a = (((n>>3)&0b11100000u32)|tag|0b00001000u32) as u8;
+            let b = (n&0xFF) as u8;
+            vec![a, b]
         } else {
-            self.names.push(needle);
-            self.names.len()
+            todo!("large numbers");
+        }
+    }
+
+    #[repr(u8)]
+    enum OpCode {
+        Label = 1,
+        FuncInfo = 2,
+        IntCodeEnd = 3,
+        Return = 19,
+        Move = 64,
+    }
+
+    // aaaa|aaaa|a000|
+    fn pad_chunk(word_size: usize, chunk: &mut Vec<u8>) {
+        let len = chunk.len();
+        let new_len = (len + word_size - 1)/word_size*word_size;
+        chunk.resize(new_len, 0)
+    }
+
+    fn encode_chunk(tag: [u8; 4], chunk: Vec<u8>) -> Vec<u8> {
+        let mut result = Vec::new();
+        result.extend(tag);
+        result.extend((chunk.len() as u32).to_be_bytes());
+        result.extend(chunk);
+        pad_chunk(4, &mut result);
+        result
+    }
+
+    // CodeChunk = <<
+    //   ChunkName:4/unit:8 = "Code",
+    //   ChunkSize:32/big,
+    //   SubSize:32/big,
+    //   InstructionSet:32/big,        % Must match code version in the emulator
+    //   OpcodeMax:32/big,
+    //   LabelCount:32/big,
+    //   FunctionCount:32/big,
+    //   Code:(ChunkSize-SubSize)/binary,  % all remaining data
+    //   Padding4:0..3/unit:8
+    // >>
+    pub fn encode_code_chunk<'a>(program: &HashMap<&'a str, usize>, atoms: &mut Atoms<'a>, labels: &mut HashMap<u32, u32>) -> Vec<u8> {
+        let mut label_count: u32 = 0;
+        let mut function_count: u32 = 0;
+
+        let mut code = Vec::new();
+        for (name, result) in program.iter() {
+            function_count += 1;
+
+            label_count += 1;
+            code.push(OpCode::Label as u8);
+            code.extend(encode_arg(Tag::U, label_count as i32));
+
+            code.push(OpCode::FuncInfo as u8);
+            code.extend(encode_arg(Tag::A, atoms.get_id("bada") as i32));
+            let name_id = atoms.get_id(name);
+            code.extend(encode_arg(Tag::A, name_id as i32));
+            code.extend(encode_arg(Tag::U, 0));
+
+            label_count += 1;
+            code.push(OpCode::Label as u8);
+            code.extend(encode_arg(Tag::U, label_count as i32));
+            labels.insert(name_id as u32, label_count);
+
+            code.push(OpCode::Move as u8);
+            code.extend(encode_arg(Tag::I, (*result) as i32));
+            code.extend(encode_arg(Tag::X, 0));
+
+            code.push(OpCode::Return as u8);
+        }
+        code.push(OpCode::IntCodeEnd as u8);
+
+        label_count += 1;
+
+        let sub_size: u32 = 16;
+        let instruction_set: u32 = 0;
+        let opcode_max: u32 = 169;
+
+        let mut chunk = Vec::new();
+        chunk.extend(sub_size.to_be_bytes());
+        chunk.extend(instruction_set.to_be_bytes());
+        chunk.extend(opcode_max.to_be_bytes());
+        chunk.extend(label_count.to_be_bytes());
+        chunk.extend(function_count.to_be_bytes());
+        chunk.extend(code);
+
+        encode_chunk(*b"Code", chunk)
+    }
+
+    // AtomChunk = <<
+    //   ChunkName:4/unit:8 = "Atom" | "AtU8",
+    //   ChunkSize:32/big,
+    //   NumberOfAtoms:32/big,
+    //   [<<AtomLength:8, AtomName:AtomLength/unit:8>> || repeat NumberOfAtoms],
+    //   Padding4:0..3/unit:8
+    // >>
+    pub fn encode_atom_chunk(atoms: &Atoms) -> Vec<u8> {
+        let mut chunk = Vec::new();
+        chunk.extend((atoms.names.len() as u32).to_be_bytes());
+        for atom in atoms.names.iter() {
+            chunk.extend((atom.len() as u8).to_be_bytes());
+            chunk.extend(atom.as_bytes());
+        }
+
+        encode_chunk(*b"AtU8", chunk)
+    }
+
+    // ImportChunk = <<
+    //   ChunkName:4/unit:8 = "ImpT",
+    //   ChunkSize:32/big,
+    //   ImportCount:32/big,
+    //   [ << ModuleName:32/big,
+    //        FunctionName:32/big,
+    //        Arity:32/big
+    //     >> || repeat ImportCount ],
+    //   Padding4:0..3/unit:8
+    // >>
+    pub fn encode_imports_chunk() -> Vec<u8> {
+        let mut chunk = Vec::new();
+        let import_count: u32 = 0;
+        chunk.extend(import_count.to_be_bytes());
+
+        encode_chunk(*b"ImpT", chunk)
+    }
+
+    // ExportChunk = <<
+    //   ChunkName:4/unit:8 = "ExpT",
+    //   ChunkSize:32/big,
+    //   ExportCount:32/big,
+    //   [ << FunctionName:32/big,
+    //        Arity:32/big,
+    //        Label:32/big
+    //     >> || repeat ExportCount ],
+    //   Padding4:0..3/unit:8
+    // >>
+    pub fn encode_exports_chunk(labels: &HashMap<u32, u32>) -> Vec<u8> {
+        let mut chunk = Vec::new();
+        let export_count: u32 = labels.len() as u32;
+        chunk.extend(export_count.to_be_bytes());
+
+        for (name_id, label) in labels.iter() {
+            chunk.extend(name_id.to_be_bytes());
+            chunk.extend(0u32.to_be_bytes());
+            chunk.extend(label.to_be_bytes());
+        }
+
+        encode_chunk(*b"ExpT", chunk)
+    }
+
+    // StringChunk = <<
+    //   ChunkName:4/unit:8 = "StrT",
+    //   ChunkSize:32/big,
+    //   Data:ChunkSize/binary,
+    //   Padding4:0..3/unit:8
+    // >>
+    pub fn encode_string_chunk() -> Vec<u8> {
+        encode_chunk(*b"StrT", vec![])
+    }
+
+    #[derive(Default)]
+    pub struct Atoms<'a> {
+        names: Vec<&'a str>,
+    }
+
+    impl<'a> Atoms<'a> {
+        fn get_id(&mut self, needle: &'a str) -> usize {
+            let result = self.names
+                .iter()
+                .enumerate()
+                .find(|(_, &name)| name == needle)
+                .map(|(index, _)| index + 1);
+            if let Some(id) = result {
+                id
+            } else {
+                self.names.push(needle);
+                self.names.len()
+            }
         }
     }
 }
@@ -244,16 +248,16 @@ fn main() -> ExitCode {
         }
     }
 
-    let mut atoms = Atoms::default();
+    let mut atoms = asm::Atoms::default();
     let mut labels: HashMap<u32, u32> = HashMap::new();
 
     let mut beam = Vec::new();
     beam.extend("BEAM".as_bytes());
-    beam.extend(encode_code_chunk(&program, &mut atoms, &mut labels));
-    beam.extend(encode_imports_chunk());
-    beam.extend(encode_exports_chunk(&labels));
-    beam.extend(encode_string_chunk());
-    beam.extend(encode_atom_chunk(&atoms));
+    beam.extend(asm::encode_code_chunk(&program, &mut atoms, &mut labels));
+    beam.extend(asm::encode_imports_chunk());
+    beam.extend(asm::encode_exports_chunk(&labels));
+    beam.extend(asm::encode_string_chunk());
+    beam.extend(asm::encode_atom_chunk(&atoms));
 
     let mut bytes: Vec<u8> = Vec::new();
     bytes.extend("FOR1".as_bytes());
